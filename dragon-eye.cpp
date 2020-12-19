119cb0f623e4e507862d2b7d58b47b52976de927
Steve Chang <stevegigijoe@yahoo.com.tw>
2020-12-19 23:49:33 +0800

    1.Supports RTSP server by gst-rtsp-server

--- dragon-eye.cpp
+++ dragon-eye.cpp
@@ -42,6 +42,13 @@ using namespace std;
 
 extern "C" {
 #include "jetsonGPIO/jetsonGPIO.h"
+
+#include "gstreamer-1.0/gst/gst.h"
+#include "gstreamer-1.0/gst/gstmessage.h"
+#include "gstreamer-1.0/gst/rtsp-server/rtsp-server.h"
+
+#include "glib-2.0/glib.h"
+#include <gstreamer-1.0/gst/app/app.h>
 }
 
 using std::chrono::high_resolution_clock;
@@ -87,6 +94,13 @@ using std::chrono::seconds;
 #define CONFIG_FILE_DIR              "/etc/dragon-eye"
 
 typedef enum { BASE_UNKNOWN, BASE_A, BASE_B, BASE_TIMER, BASE_ANEMOMETER } BaseType_t;
+
+/*
+*
+*/
+
+static bool bShutdown = false;
+static bool bPause = true;
 
 /*
 *
@@ -407,6 +421,175 @@ void FrameQueue::reset()
 
 static FrameQueue videoOutputQueue;
 
+/*
+*
+*/
+
+typedef struct {
+    Mat lastFrame;
+    int numberFrames;
+    GstClockTime timestamp;
+} RtspServerContext;
+
+// should be private data of c++ class
+static int numberFrames = 0;
+static Mat lastFrame;
+
+/* called when we need to give data to appsrc */
+static void
+need_data (GstElement * appsrc, guint unused, RtspServerContext *ctx)
+{
+    ctx->lastFrame = videoOutputQueue.pop();
+
+    GstBuffer *buffer;
+    uint64_t size=CAMERA_WIDTH * CAMERA_HEIGHT * 4; // Image size * deth of BGRx;
+    GstFlowReturn ret;
+    buffer = gst_buffer_new_allocate (NULL, size, NULL);
+    GstMapInfo map;
+    gint8 *raw;
+
+    gst_buffer_map (buffer, &map, GST_MAP_WRITE); // make buffer writable
+    raw = (gint8 *)map.data;
+
+    for (int i = 0; i<CAMERA_HEIGHT; i++) {
+        Vec3b* ptr = ctx->lastFrame.ptr<Vec3b>(i);
+        for (int j = 0; j<CAMERA_WIDTH; j++) {
+            uint64_t offset = ((i*CAMERA_WIDTH)+j)*4;
+            raw[offset] = ptr[j][0];
+            raw[offset+1] = ptr[j][1];
+            raw[offset+2] = ptr[j][2];
+            raw[offset+3] = 127;
+        }
+    }
+    gst_buffer_unmap (buffer, &map);
+
+    /* increment the timestamp every 1/FPS second */
+    GST_BUFFER_PTS (buffer) = ctx->timestamp;
+    GST_BUFFER_DURATION (buffer) = gst_util_uint64_scale_int (1, GST_SECOND, 30);
+    ctx->timestamp += GST_BUFFER_DURATION (buffer);
+
+    g_signal_emit_by_name (appsrc, "push-buffer", buffer, &ret);
+    gst_buffer_unref (buffer);
+}
+
+/* called when a new media pipeline is constructed. CAMERA_WIDTHe can query the
+ * pipeline and configure our appsrc */
+static void
+media_configure (GstRTSPMediaFactory * factory, GstRTSPMedia * media, gpointer user_data)
+{
+    // should be incremented once on each frame for timestamping
+
+    GstElement *element, *appsrc;
+    RtspServerContext *ctx;
+
+    /* get the element used for providing the streams of the media */
+    element = gst_rtsp_media_get_element (media);
+
+    /* get our appsrc, we named it 'mysrc' with the name property */
+    appsrc = gst_bin_get_by_name_recurse_up (GST_BIN (element), "mysrc");
+
+    /* this instructs appsrc that we will be dealing with timed buffer */
+    gst_util_set_object_arg (G_OBJECT (appsrc), "format", "time");
+    /* configure the caps of the video */
+    g_object_set (G_OBJECT (appsrc), "caps",
+    gst_caps_new_simple ("video/x-raw",
+                "format", G_TYPE_STRING, "BGRx",
+                "width", G_TYPE_INT, CAMERA_WIDTH,
+                "height", G_TYPE_INT, CAMERA_HEIGHT,
+                "framerate", GST_TYPE_FRACTION, 30, 1, NULL), NULL);
+
+    ctx = g_new0 (RtspServerContext, 1);
+    ctx->timestamp = 0;
+    //ctx->lastFrame = &lastFrame;
+    ctx->numberFrames = numberFrames;
+
+    /* make sure ther datais freed when the media is gone */
+    g_object_set_data_full (G_OBJECT (media), "my-extra-data", ctx, (GDestroyNotify) g_free);
+
+    /* install the callback that will be called when a buffer is needed */
+    g_signal_connect (appsrc, "need-data", (GCallback) need_data, ctx);
+    gst_object_unref (appsrc);
+    gst_object_unref (element);
+}
+
+#include <glib-object.h>
+#include <glib-unix.h>
+
+// This callback will be inovked when this process receives SIGINT or SIGTERM.
+gboolean TerminationSignalCallback(gpointer data) {
+  cout << "Received a signal to terminate the daemon";
+  GMainLoop* loop = reinterpret_cast<GMainLoop*>(data);
+  g_main_loop_quit(loop);
+  // This function can return false to remove this signal handler as we are
+  // quitting the main loop anyway.
+  bShutdown = true;
+
+  return false;
+}
+
+int gst_rtsp_server_init()
+{
+    GMainLoop *loop;
+    GstRTSPServer *server;
+    GstRTSPMountPoints *mounts;
+    GstRTSPMediaFactory *factory;
+
+    char *args[] = {
+        (char*)"dragon-eye",
+        NULL
+    };
+    int argv = 0;
+    gst_init (&argv, (char ***)&args);
+
+    loop = g_main_loop_new (NULL, FALSE);
+
+    // Set up a signal handler for handling SIGINT and SIGTERM.
+    g_unix_signal_add(SIGINT, TerminationSignalCallback, loop);
+    //g_unix_signal_add(SIGTERM, TerminationSignalCallback, loop);
+    
+    /* create a server instance */
+    server = gst_rtsp_server_new ();
+    /* get the mount points for this server, every server has a default object
+    * that be used to map uri mount points to media factories */
+    mounts = gst_rtsp_server_get_mount_points (server);
+
+    /* make a media factory for a test stream. The default media factory can use
+    * gst-launch syntax to create pipelines.
+    * any launch line works as long as it contains elements named pay%d. Each
+    * element with pay%d names will be a stream */
+    factory = gst_rtsp_media_factory_new ();
+    gst_rtsp_media_factory_set_launch (factory,
+//          "( appsrc name=mysrc is-live=true ! videoconvert ! omxh265enc ! rtph265pay mtu=1400 name=pay0 pt=96 )");
+        "appsrc name=mysrc is-live=true ! videoconvert ! \
+nvvidconv flip-method=1 ! omxh265enc control-rate=2 bitrate=4000000 ! rtph265pay mtu=1400 name=pay0 pt=96 )");
+
+    gst_rtsp_media_factory_set_shared (factory, TRUE);
+    /* notify when our media is ready, This is called whenever someone asks for
+    * the media and a new pipeline with our appsrc is created */
+    g_signal_connect (factory, "media-configure", (GCallback) media_configure, NULL);
+
+    /* attach the test factory to the /test url */
+    gst_rtsp_mount_points_add_factory (mounts, "/test", factory);
+
+    /* don't need the ref to the mounts anymore */
+    g_object_unref (mounts);
+
+    /* attach the server to the default maincontext */
+    gst_rtsp_server_attach (server, NULL);
+
+    /* start serving */
+    g_print ("stream ready at rtsp://127.0.0.1:8554/test\n");
+    g_main_loop_run (loop);
+
+    return 0;
+}
+
+static thread rtspServerThread;
+
+/*
+*
+*/
+
 void VideoOutputThread(BaseType_t baseType, bool isVideoOutputScreen, bool isVideoOutputFile, 
     bool isVideoOutputRTP, const char *rtpRemoteHost, uint16_t rtpRemotePort, 
     bool isVideoOutputHLS, bool isVideoOutputRTSP, int width, int height)
@@ -503,17 +686,7 @@ hlssink playlist-location=/tmp/playlist.m3u8 location=/tmp/segment%%05d.ts targe
     }
 
     if(isVideoOutputRTSP) {
-        snprintf(gstStr, STR_SIZE, "appsrc is-live=true ! video/x-raw, format=(string)BGR ! \
-videoconvert ! video/x-raw, format=(string)I420, framerate=(fraction)%d/1 ! \
-nvvidconv flip-method=1 ! omxh265enc control-rate=2 bitrate=4000000 ! video/x-h265, stream-format=byte-stream ! \
-h265parse ! shmsink socket-path=/tmp/h265.sock sync=true wait-for-connection=0 ", 30);
-        outRTSP.open(gstStr, VideoWriter::fourcc('X', '2', '6', '4'), 30, Size(CAMERA_WIDTH, CAMERA_HEIGHT));
-        cout << endl;
-        cout << gstStr << endl;
-        cout << endl;
-        cout << "*** Start RTSP video ***" << endl;        
-
-        //system("./test-launch \"shmsrc socket-path=/tmp/h265.sock ! video/x-h264, stream-format=byte-stream, width=640, height=480, framerate=30/1 ! h264parse ! video/x-h264, stream-format=byte-stream ! rtph264pay pt=96 name=pay0 \"");
+        rtspServerThread = thread(&gst_rtsp_server_init);
     }
 
     videoOutputQueue.reset();
@@ -531,8 +704,8 @@ h265parse ! shmsink socket-path=/tmp/h265.sock sync=true wait-for-connection=0 "
                 outRTP.write(frame);
             if(isVideoOutputHLS)
                 outHLS.write(frame);
-            if(isVideoOutputRTSP)
-                outRTSP.write(frame);
+            //if(isVideoOutputRTSP)
+            //    outRTSP.write(frame);
             
             steady_clock::time_point t2 = steady_clock::now();
             double secs(static_cast<double>(duration_cast<seconds>(t2 - t1).count()));
@@ -565,7 +738,8 @@ h265parse ! shmsink socket-path=/tmp/h265.sock sync=true wait-for-connection=0 "
         if(isVideoOutputRTSP) {
             cout << endl;
             cout << "*** Stop RTSP video ***" << endl;
-            outRTSP.release();
+            //outRTSP.release();
+            rtspServerThread.join();
         }
     }    
 }
@@ -1376,13 +1550,6 @@ nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! vide
 };
 
 static Camera camera; 
-
-/*
-*
-*/
-
-static bool bShutdown = false;
-static bool bPause = true;
 
 /*
 *
